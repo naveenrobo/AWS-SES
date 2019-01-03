1aad4dcbe8d23041c14302d1c73d16d547347ba4
Naveen <naveenrobotics@live.in>
2019-01-03 10:48:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var AWS = require('aws-sdk');
+AWS.config.update({
+    region: 'us-east-1'
+});
+
+var params = {
+    Destination: {
+        ToAddresses: [
+            'muthu.vangaliappan@katomaran.com',
+        ]
+    },
+    Message: {
+        Body: {
+            Html: {
+                Charset: "UTF-8",
+                Data: "<h1>test</h1>"
+            },
+            Text: {
+                Charset: "UTF-8",
+                Data: "test"
+            }
+        },
+        Subject: {
+            Charset: 'UTF-8',
+            Data: 'Test email'
+        }
+    },
+    Source: 'hello@naveens.co.in',
+    ReplyToAddresses: [
+        'naveenrobotics@live.in',
+    ],
+};
+
+var sendPromise = new AWS.SES({
+    apiVersion: '2010-12-01'
+}).sendEmail(params).promise();
+
+(async function () {
+    try {
+        let data = await sendPromise;
+        console.log(data.MessageId);
+    } catch (err) {
+        console.log(err);
+    }
+})();
